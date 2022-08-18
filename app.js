69afb95740cec3a545ed3559c2ab4d39ada6054f
Marion Knudson <marionknudson@gmail.com>
2022-08-18 14:21:19 -0600

    add start script

--- app.js
+++ app.js
@@ -102,6 +102,8 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render('error', { err });
 });
 
-app.listen(3000, () => {
-  console.log('listening on port 3000');
+const port = process.env.PORT || 3000
+
+app.listen(, () => {
+  console.log(`listening on port ${port}`);
 });
